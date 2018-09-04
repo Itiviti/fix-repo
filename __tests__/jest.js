f129223ac455c2addc47c1f37916f21a9816eb7c
Peter Ng <ngyukman@gmail.com>
2018-09-04 22:58:02 +0800

    try js

--- __tests__/jest.js
+++ __tests__/jest.js
@@ -1,27 +1,26 @@
-import { Category, Component, Field, Message } from '../src/model';
-import { Fix42, Fix44, Fix50 } from './../src';
+import { Fix42, Fix44, Fix50 } from '../src';
 
 const fixRepos = [ Fix42, Fix44, Fix50 ];
 
 fixRepos.forEach(fixRepo => {
     describe("For each Fix Repository", () => {
-        test.each(fixRepo.categories)('category %s property is not throwing', (category: Category) => {
+        test.each(fixRepo.categories)('category %s property is not throwing', category => {
             expect(category.section);
         });
 
-        test.each(fixRepo.fields)('field %s property is not throwing', (field: Field) => {
+        test.each(fixRepo.fields)('field %s property is not throwing', field => {
             expect(field.type);
             expect(field.baseCategroy);
         });
 
-        test.each(fixRepo.messages)('message %s property is not throwing', (message: Message) => {
+        test.each(fixRepo.messages)('message %s property is not throwing', message => {
             expect(message.category);
             message.structures.forEach(structure => {
                 expect(structure.type).toBeDefined();
             });
         });
 
-        test.each(fixRepo.components)('component %s property is not throwing', (component: Component) => {
+        test.each(fixRepo.components)('component %s property is not throwing', component => {
             component.structures.forEach(structure => {
                 expect(structure.type).toBeDefined();
             });
