0cd71a882f4dd11d87fb4057e769e9fc2c487bcb
Peter Ng <ngyukman@gmail.com>
2018-09-05 23:32:47 +0800

    Improve coverage

--- __tests__/spec.ts
+++ __tests__/spec.ts
@@ -10,18 +10,34 @@ fixRepos.forEach(fixRepo => {
         });
 
         test.each(fixRepo.fields)('field %s property is not throwing', (field: Field) => {
+            const repoField: any = field.toJSON();
+
             expect(field.type);
             expect(field.baseCategroy);
-            expect(field.added);
-            expect(field.addedEP);
-            expect(field.updated);
-            expect(field.updatedEP);
-            expect(field.deprecated);
-            expect(field.deprecatedEP);
+            expect(field.added).toEqual(repoField.added);
+            expect(field.addedEP).toEqual(repoField.addedEP);
+            expect(field.updated).toEqual(repoField.updated);
+            expect(field.updatedEP).toEqual(repoField.updatedEP);
+            expect(field.deprecated).toEqual(repoField.deprecated);
+            expect(field.deprecatedEP).toEqual(repoField.deprecatedEP);
+            expect(field.unionDataType).toEqual(repoField.unionDataType);
+            expect(field.lengthId).toEqual(repoField.lengthId);
+            expect(field.lengthName).toEqual(repoField.lengthName);
+            expect(field.abbrName).toEqual(repoField.abbrName);
+            expect(field.issue).toEqual(repoField.issue);
+            expect(field.documentation).toEqual(repoField.documentation);
+            expect(field.baseCategoryAbbrName).toEqual(repoField.baseCategoryAbbrName);
         });
 
         test.each(fixRepo.messages)('message %s property is not throwing', (message: Message) => {
+            const repoMessage: any = message.toJSON();
+
             expect(message.category);
+            expect(message.id).toEqual(repoMessage.id);
+            expect(message.abbrName).toEqual(repoMessage.abbrName);
+            expect(message.msgType).toEqual(repoMessage.msgType);
+            expect(message.documentation).toEqual(repoMessage.documentation);
+            expect(message.section).toEqual(repoMessage.section);
             message.structures.forEach(structure => {
                 expect(structure.type).toBeDefined();
             });
